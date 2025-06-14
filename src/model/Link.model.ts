255937406e1f0f21f2bf113479c8333e9f392d7a
anshumancodes <anshumanprof01@gmail.com>
2025-06-14 23:03:03 +0530

    add Link model with user reference and hash field

---
+++ src/model/Link.model.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Link extends Document {
+  hash: string;
+  userId: mongoose.Types.ObjectId;
+}
+
+const linkSchema = new Schema<Link>({
+  hash: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+
+const LinkModel = mongoose.model<Link>("Link", linkSchema);
+export default LinkModel;
