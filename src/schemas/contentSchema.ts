c5ad0f4d51b8f9668c9785ce9fc2cfa9f2ddca8f
anshumancodes <anshumanprof01@gmail.com>
2025-08-05 04:20:59 +0530

    feat: add content validation schema with zod

---
+++ src/schemas/contentSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const contentSchema = z.object({
+  link: z.string(),
+  type: z.string().min(3, "Type must be at least 3 characters long"),
+  title: z.string().min(4, "Title must be at least 4 characters long"),
+  tags: z.array(z.string()),
+});
+
+export default contentSchema;
