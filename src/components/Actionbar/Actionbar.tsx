e7cba8f0790c28df19490d6d39afca9774e52b56
anshumancodes <anshumanprof01@gmail.com>
2025-08-19 13:34:56 +0530

    feat(ui): add filter dropdown and integrate CreateModal trigger in Actionbar
    - Implemented filter dropdown for content types (UI) (all, docs, tweets, videos)
    
    - Connected 'Add Content' button to useCreateContentModal store
    
    - Marked Actionbar as a client component

--- src/components/Actionbar/Actionbar.tsx
+++ src/components/Actionbar/Actionbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Funnel, Plus, Share2 } from "lucide-react";
 import {
   Select,
@@ -7,37 +8,46 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+import { useCreateContentModal } from "@/context/Content.store";
+
 export default function Actionbar() {
+  const openModal = useCreateContentModal((state) => state.openModal);
+
   return (
     <div className="flex flex-wrap justify-between items-center px-6 py-4 shadow-sm border-b border-gray-600 gap-3">
-      
       <div className="flex items-center gap-8">
         <p className="text-xl font-semibold">Vault</p>
-
         {/* Filter */}
         <div className="flex items-center gap-2 px-4 py-1 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 transition">
-          <Funnel className="w-4 h-4 text-gray-700" />
+          {" "}
+          <Funnel className="w-4 h-4 text-gray-700" />{" "}
           <Select defaultValue="">
-            <SelectTrigger className="w-fit bg-transparent border-0  text-sm text-gray-700">
-              <SelectValue placeholder="All Content" />
-            </SelectTrigger>
+            {" "}
+            <SelectTrigger className="w-fit bg-transparent border-0 text-sm text-gray-700">
+              {" "}
+              <SelectValue placeholder="All Content" />{" "}
+            </SelectTrigger>{" "}
             <SelectContent>
-              <SelectItem value="all">All Content </SelectItem>
-              <SelectItem value="docs">Docs</SelectItem>
-              <SelectItem value="tweets">Tweets</SelectItem>
-              <SelectItem value="videos">Videos</SelectItem>
-            </SelectContent>
-          </Select>
+              {" "}
+              <SelectItem value="all">All Content </SelectItem>{" "}
+              <SelectItem value="docs">Docs</SelectItem>{" "}
+              <SelectItem value="tweets">Tweets</SelectItem>{" "}
+              <SelectItem value="videos">Videos</SelectItem>{" "}
+            </SelectContent>{" "}
+          </Select>{" "}
         </div>
       </div>
 
-      {/* Right side: Action buttons */}
       <div className="flex gap-3">
         <button className="flex items-center gap-2 px-4 py-2 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 transition">
           <Share2 className="w-4 h-4" />
           Share Brain
         </button>
-        <button className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+
+        <button
+          className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+          onClick={openModal}
+        >
           <Plus className="w-4 h-4" />
           Add Content
         </button>
