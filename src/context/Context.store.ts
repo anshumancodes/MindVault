77a4a8fba7f6b26f2cf2893c5ca9b5e1781a8b4d
anshumancodes <anshumanprof01@gmail.com>
2025-08-29 04:15:22 +0530

    refactor(store): rename Content.store.ts to Context.store.ts and add settings modal state
    - Renamed file for clarity
    - Added `useOpenSettingsModal` Zustand store with open/close handlers

---
+++ src/context/Context.store.ts
@@ -0,0 +1,48 @@
+import { create } from "zustand";
+
+type CreateContentModalState = {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+};
+export const useCreateContentModal = create<CreateContentModalState>((set) => ({
+  isOpen: false,
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false }),
+}));
+
+type OpenUserModalState = {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+};
+
+export const useUserModal= create<OpenUserModalState>((set) => ({
+  isOpen: false,
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false }),
+}));
+
+type OpenShareModalState = {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+};
+
+export const useShareModal= create<OpenShareModalState>((set) => ({
+  isOpen: false,
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false }),
+}));
+
+type OpenSettingsModalState = {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+};
+
+export const useOpenSettingsModal= create<OpenSettingsModalState>((set) => ({
+  isOpen: false,
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false }),
+}));
