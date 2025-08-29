6e284247173e2fcd27295e16fd5ac6b72e5dd8df
anshumancodes <anshumanprof01@gmail.com>
2025-08-29 17:46:44 +0530

    feat(api): add POST /content route for creating new content

---
+++ src/app/api/v1/content/create/route.ts
@@ -0,0 +1,51 @@
+import ContentModel from "@/models/content";
+import { NextResponse, NextRequest } from "next/server";
+import contentSchema from "@/schemas/contentSchema";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { link, description, title, type, tags } = body;
+
+    const parsedData = contentSchema.safeParse({
+      link,
+      description,
+      title,
+      type,
+      tags,
+    });
+    if (!parsedData.success) {
+      return NextResponse.json(
+        {
+          message: "validation failed!",
+          errors: parsedData.error.flatten().fieldErrors,
+        },
+        { status: 400 }
+      );
+    }
+    const createContent = await ContentModel.create({
+      link,
+      title,
+      description,
+      type,
+      tags,
+    });
+
+    return NextResponse.json(
+      {
+        message: "added content successfully!",
+        content: createContent, 
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Unexpected server error: couldnt create content!",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
