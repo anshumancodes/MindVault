ef84c47728fae2bb725f165e8ed05090aeca5f10
anshumancodes <anshumanprof01@gmail.com>
2025-08-29 16:48:55 +0530

    feat(schema): add description field to content schema
    - Added `description` with min length validation (10 chars)
    - Keeps existing validations for link, type, title, and tags

--- src/schemas/contentSchema.ts
+++ src/schemas/contentSchema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const contentSchema = z.object({
   link: z.string(),
   type: z.string().min(3, "Type must be at least 3 characters long"),
+  description:z.string().min(10,"description must be atleast 10 characters long!"),
   title: z.string().min(4, "Title must be at least 4 characters long"),
   tags: z.array(z.string()),
 });
