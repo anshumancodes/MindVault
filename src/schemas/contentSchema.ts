bbaa67e07462a6df85878d8f70956315a29d6af5
anshumancodes <anshumanprof01@gmail.com>
2025-09-11 02:58:53 +0530

    fix(schema): make link, description & tags optional-m - Updated  to:   - Allow  as optional with URL validation   - Make  optional or empty string, while keeping min length rule when provided   - Allow  array to be optional - Improves flexibility for content creation without requiring all fields

--- src/schemas/contentSchema.ts
+++ src/schemas/contentSchema.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 
 const contentSchema = z.object({
-  link: z.string(),
+  link: z.string().url().optional(),
   type: z.string().min(3, "Type must be at least 3 characters long"),
-  description:z.string().min(10,"description must be atleast 10 characters long!"),
+  description: z
+    .string()
+    .min(10, "Description must be at least 10 characters long")
+    .optional()
+    .or(z.literal("")),
   title: z.string().min(4, "Title must be at least 4 characters long"),
-  tags: z.array(z.string()),
+  tags: z.array(z.string()).optional(),
 });
 
 export default contentSchema;
