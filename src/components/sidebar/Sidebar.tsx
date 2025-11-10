c9b07d06354c0ce9f64ea81d20a859bba83d2d92
anshumancodes <anshumanprof01@gmail.com>
2025-11-11 00:31:24 +0530

    refactor(sidebar): add login prompt for unauthenticated users and improve layout spacing

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronDown, X } from "lucide-react";
+import {  X } from "lucide-react";
 import { useUserModal, useOpenSidebar } from "@/context/Context.store";
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
@@ -8,7 +8,7 @@ import { useSession } from "next-auth/react";
 export default function Sidebar() {
   const { openModal } = useUserModal();
   const { isOpen, closeSidebar } = useOpenSidebar();
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   // Close sidebar when pressing Escape (wasnt nesccary byt hey)
   useEffect(() => {
@@ -81,11 +81,11 @@ export default function Sidebar() {
         {/* Profile Section */}
         <div
           onClick={openModal}
-          className="mt-auto border-t border-neutral-800 px-3 py-3 flex items-center justify-between cursor-pointer hover:bg-neutral-800 transition rounded-lg"
+          className="mt-auto border-t border-neutral-800 px-2 py-3 flex items-center justify-between cursor-pointer hover:bg-neutral-800 transition rounded-lg"
         >
           {/* Avatar + Info */}
-          <div className="flex items-center gap-3">
-            <div className="bg-gray-200 text-gray-800 font-medium px-2 py-2 rounded-full flex items-center justify-center h-[40px] w-[40px]">
+          <div className="flex items-center gap-2">
+            <div className="bg-gray-200 text-gray-800 font-medium px-3 py-2 rounded-full flex items-center justify-center h-[30px] w-[30px]">
               {user?.name
                 ?.split(" ") // Split the full name into parts
                 .slice(0, 2) // Take first two (first + last)
@@ -94,12 +94,20 @@ export default function Sidebar() {
             </div>
 
             <div className="flex flex-col leading-tight">
-              <p className="text-sm font-medium text-white">{user?.name}</p>
-              <p className="text-xs text-gray-400">{user?.email}</p>
+              {status === "unauthenticated" ? (
+                <div>
+                  <p className="text-base text-white">
+                    Login to access your Vault
+                  </p>
+                </div>
+              ) : (
+                <>
+                  <p className="text-sm text-white">{user?.name}</p>
+                  <p className="text-xs text-gray-400">{user?.email}</p>
+                </>
+              )}
             </div>
           </div>
-
-          <ChevronDown className="w-5 h-5 text-gray-400" />
         </div>
       </aside>
     </>
